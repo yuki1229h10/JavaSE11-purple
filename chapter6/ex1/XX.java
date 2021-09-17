7bad065e379d055bf53ad0dad4133feb13af1ed9
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2021-09-17 21:22:16 +0900

    first commit

---
+++ chapter6/ex1/XX.java
@@ -0,0 +1,4 @@
+public class XX {
+    void method() {
+    }
+}
---
+++ chapter6/ex1/YY.java
@@ -0,0 +1,15 @@
+public class YY extends XX {
+    // public void method() {}
+    // void method(int i, String s) {
+
+    // }
+    // void method(Object o) {
+
+    // }
+    // protected void method() {
+
+    // }
+    // private void method() {
+
+    // }
+}
